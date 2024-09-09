dcd67d265cf1248cdd4a0ed66622419b0a7d839e
Abhay Balip <121150307+abhaybalip@users.noreply.github.com>
2024-09-09 20:05:11 +0530

    images modified

--- src/Component/Testimonial.jsx
+++ src/Component/Testimonial.jsx
@@ -5,14 +5,6 @@ import '../Asset/css-files/Testimonial/Testimonial.css'
 
 const Testimonial = () => {
     const image = [
-        { id: 0, src: require('../Asset/Image/menu-icon.png') },
-        { id: 1, src: require('../Asset/Image/suvidha-logo.png') },
-        { id: 2, src: require('../Asset/Image/top-icon.png') },
-        { id: 3, src: require('../Asset/Image/HomePage/ieee-bombay.jpg') },
-        { id: 4, src: require('../Asset/Image/HomePage/ieee-xplore.jpg') },
-        { id: 5, src: require('../Asset/Image/HomePage/ieee.jpg') },
-        { id: 6, src: require('../Asset/Image/HomePage/Button/NextButton.png') },
-        { id: 7, src: require('../Asset/Image/HomePage/Button/PrevButton.png') },
     ]
 
     useEffect(() => {
@@ -31,7 +23,7 @@ const Testimonial = () => {
             panel.appendChild(firstImgCopy)
 
             // scroll to next image
-            panel.scroll({left: imgWidth, behavior: 'smooth'})
+            panel.scroll({ left: imgWidth, behavior: 'smooth' })
 
             // remove first child
             panel.removeChild(panel.firstElementChild)
@@ -50,15 +42,18 @@ const Testimonial = () => {
         <div className='home-s4'>
             <div className='home-s4-t1'>Testimonial Views</div>
             <div className='home-s4-panel'>
-                <div id='panel-row' className='panel-row'>
-                    {image.map((value) => (
-                        <div className='panel-row-box'>
+                {
+                    Image.length > 0 ?
+                        <div id='panel-row' className='panel-row'>
+                            {image.map((value) => (
+                                <div className='panel-row-box'>
 
-                            <img key={value.id} src={value.src} alt={value.id} className='panel-row-img' />
 
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
+                        : null
+                }
             </div>
         </div>
     );
